8d46093563e252726c038d8c0ffad3668b28c40b
Web Janv 2015 <Web Janv 2015>
2017-04-10 14:26:02 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title></title>
+		<link rel="stylesheet" href="css/bootstrap-grid.css">
+		<link rel="stylesheet" href="css/bootstrap-reboot.css">
+		<link rel="stylesheet" href="css/bootstrap.css">
+		<link rel="stylesheet" href="css/style.css">
+		<link href="https://fonts.googleapis.com/css?family=Merriweather|Open+Sans" rel="stylesheet">
+	</head>
+
+	<body>
+		<header>
+		<div class="container">
+			<h1 class="mb-md">Au <span class="brown">Café des Ours</span>,<br> Nous torréfions le Café et les <span class="brown">Sensations</span></h1>
+			<nav>
+			<a href="index.php">Accueil</a>
+			<a href="">Boutique</a>
+			<a href="">Recettes</a>
+			<a href="">Notre histoire</a>
+			<a href="">Nos variétés de café</a>
+			<a href="">Contact</a>
+			</nav>
+
+			<a href="" id="logo"><img src="img/logo.png" alt=""></a>
+
+			<a href="" id="h_cafe">L'histoire de nos café <span class="brown">&#8594;</span></a>
+			</div>
+		</header>
+	</body>
+</html>
